7e1e566e48034f7946475e85d514a0d54555f0a0
ArtaroPC <a.tossapol.j@gmail.com>
2023-09-27 17:20:01 +0700

    add: session, auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 import { NavLinks } from "@/constants/index";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -22,9 +23,18 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rouned-full"
+                alt={session.user.name}
+              />
+            )}
+
             <Link href="create-project">Share Work</Link>
           </>
         ) : (
